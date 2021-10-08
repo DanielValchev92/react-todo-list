4d7c7295a498e547a377def0286e260f55083557
DanielValchev92 <dvalchev92@gmail.com>
2021-10-08 22:31:01 +0300

    setState + creating handlers in the form component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>My Todo List</h1>
       </header>
 
-      <Form />
+      <Form setInputText={setInputText} />
       <TodoList />
 
     </div>
